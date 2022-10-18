af3f69493ae4747294c770f811a62097d16f36a1
MathCoder <mathieufc31@gmail.com>
2022-10-18 15:07:26 +0200

    update secret key

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const posts = require("./routes/posts");
 
 app.use("/posts", posts);
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
-  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
+mongoose.connect(
+  "mongodb+srv://MatDev:LgfC8my3rxllpe3X@cluster0.7cwnoib.mongodb.net/shadowdb?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () =>
+    console.log(
+      `Connection ${new Date().getHours()}:${new Date().getMinutes()}`
+    )
 );
 app.listen(process.env.PORT || 4000);
